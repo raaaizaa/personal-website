0581f33d46fb578a46a98ec35975e6c75ce10ce5
raaizar <rahmanraiza@gmail.com>
2023-07-22 17:41:57 +0700

    feat: portfolio page done style: re-styling

--- components/pages/home/home-page.tsx
+++ components/pages/home/home-page.tsx
@@ -1,19 +1,11 @@
-'use client'
-import { TextMontserrat } from '@/components/atoms/typography/text-montserrat/text-montserrat';
-import Image from 'next/image';
-import React from 'react';
-import portrait from 'public/images/portrait.png'
+'use client';
+
 import Title from '@/components/molecules/home/title';
 
 export default function Homepage() {
   return (
-    <div className=' lg:inline-flex h-[93%] w-screen'>
-        <div className='flex flex-col lg:w-[50%] justify-center ps-4 sm:ps-8 md:ps-12 lg:ps-20 xl:ps-32 py-32 flex-grow transition-all '>
-          <Title />
-        </div>
-        <div className='flex flex-grow justify-center items-center lg:w-[50%]'>
-            <Image src={portrait} width={600} height={600} alt='' />
-        </div>
+    <div className='h-[93%] p-8 lg:p-28 xl:p-32 flex flex-col justify-center items-start'>
+      <Title />
     </div>
   );
 }
