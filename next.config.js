a2188120c93fede4e57c5b163fd689dea3d611fa
raaizar <rahmanraiza@gmail.com>
2023-08-06 17:27:17 +0700

    v1 bug fixes

--- next.config.js
+++ next.config.js
@@ -1,21 +1,16 @@
 /** @type {import('next').NextConfig} */
 const fs = require('fs');
 
-
 const dotenv = require('dotenv');
 
 const env = dotenv.parse(fs.readFileSync('.env'));
 
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    SERVER: process.env.SERVER
-  },
-  images: {
-    domains: ['i.scdn.co', 'lastfm.freetls.fastly.net', 'cdn.myanimelist.net']
-}
-}
+    reactStrictMode: true,
+    env: env,
+    images: {
+        domains: ['i.scdn.co', 'lastfm.freetls.fastly.net', 'cdn.myanimelist.net']
+    }
+};
 
-
-
-module.exports = nextConfig
+module.exports = nextConfig;
