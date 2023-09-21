07737631d702e7ff30bb22b7d2ce3b0d0dc0030e
raaizar <rahmanraiza@gmail.com>
2023-09-21 17:17:13 +0700

    v2: now simplified

--- components/molecules/home/title.tsx
+++ components/molecules/home/title.tsx
@@ -1,33 +1,25 @@
 import React from 'react'
 import '@/styles/globals.css'
-import { TextP5 } from '@/components/atoms/typography/text-p5/text-p5'
-import { TextTooDrunk } from '@/components/atoms/typography/text-linotype/text-linotype'
-import {
-  TextHumanError,
-  TextHumanErrorFlag,
-} from '@/components/atoms/typography/text-human-error/text-human-error'
 import Image from 'next/image'
-import hello from '@/public/images/homepage/title/Hello.png'
+import hello from '@/public/images/homepage/title/hello.png'
 import my from '@/public/images/homepage/title/my.png'
 import name from '@/public/images/homepage/title/name.png'
 import is from '@/public/images/homepage/title/is.png'
 import raiza from '@/public/images/homepage/title/raiza.png'
-import portrait from '@/public/images/portrait.png'
 
 export default function Title() {
   return (
     <div className="justify-center lg:justify-start xl:justify-start items-center">
-      <div className='flex justify-center lg:justify-start xl:justify-start'>
-      <Image src={hello} alt="hello" />
-
+      <div className="flex justify-center lg:justify-start xl:justify-start">
+        <Image src={hello} alt="hello" />
       </div>
       <div className="flex justify-center lg:justify-start xl:justify-start items-center">
         <Image src={my} alt="my" />
         <Image src={name} alt="name" />
       </div>
       <div className="flex justify-center lg:justify-start xl:justify-start items-center">
-          <Image src={is} alt="is" />
-          <Image src={raiza} alt="raiza" />
+        <Image src={is} alt="is" />
+        <Image src={raiza} alt="raiza" />
       </div>
     </div>
   )
